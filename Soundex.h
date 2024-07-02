3ed640a66b80e6554f0ef98cb5238a8253a4d32d
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-02 23:26:40 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -20,18 +20,27 @@ void generateSoundex(const char *name, char *soundex) {
     int len = (int)strlen(name);
     int sIndex = 1;
     char prevCode = '0';
+
     soundex[0] = (char)toupper(name[0]);
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
+    for (int i = 1; i < len; i++) {
         char code = getSoundexCode(name[i]);
+
+        // If code is valid and different from the previous one
         if (code != '0' && code != prevCode) {
             soundex[sIndex++] = code;
-            prevCode = code;
-        } else {
-            prevCode = code;
+        }
+
+        // Update prevCode regardless of whether the code was added
+        prevCode = code;
+
+        // Stop if soundex string is full
+        if (sIndex == 4) {
+            break;
         }
     }
 
+    // Fill remaining spaces with '0'
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
     }
