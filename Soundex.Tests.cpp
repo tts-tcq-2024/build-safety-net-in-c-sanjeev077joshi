0b8f2a86ba6b7287f59526f62caab9cf15459ceb
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-02 22:49:09 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,8 +2,43 @@
 #include "Soundex.h"
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
   char soundex[5];
+ 
   generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+  ASSERT_EQ(soundex,"A200");
+
+ generateSoundex("Smith", soundex);
+ ASSERT_EQ(soundex,"S530");
+}
+
+TEST(Soundex, ShortNameTest) {
+
+  char soundex[5];
+
+  generateSoundex("A", soundex);
+  ASSERT_EQ(soundex,"A000");
+}
+
+TEST(Soundex, casesensitivelowercase)
+{
+	  char soundex[5];
+
+	  generateSoundex("johnson", soundex);
+	  ASSERT_EQ(soundex,"J525");
+}
+
+TEST(Soundex, specialcharacters)
+{
+	  char soundex[5];
+
+	  generateSoundex("%jkl", soundex);
+	  ASSERT_EQ(soundex,"%240");
+}
+
+TEST(Soundex, longname)
+{
+	  char soundex[5];
+
+	  generateSoundex("Rameshkumar", soundex);
+	  ASSERT_EQ("R525", soundex);
 }
